05eaedf0cc8ad1b992c885b3bf6c8c65982b366f
ssinchak <shs355@pitt.edu>
2024-06-06 11:29:49 -0700

    creating package files

---
+++ flexibility/flexibility/__init__.py
@@ -0,0 +1,7 @@
+"""manipulate strings"""
+
+# Add imports here
+from .stretch import *
+
+
+from ._version import __version__
---
+++ flexibility/flexibility/tests/test_flexibility.py
@@ -0,0 +1,15 @@
+"""
+Unit and regression test for the flexibility package.
+"""
+
+# Import package, test suite, and other packages as needed
+import sys
+
+import pytest
+
+import flexibility
+
+
+def test_flexibility_imported():
+    """Sample test, will always pass so long as import statement worked."""
+    assert "flexibility" in sys.modules
