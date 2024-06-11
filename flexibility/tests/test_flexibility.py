7584ce40aebce21444f76e27d67a1c2a5a36c2f2
ssinchak <shs355@pitt.edu>
2024-06-11 13:49:49 -0700

    fix test flex

--- flexibility/tests/test_flexibility.py
+++ flexibility/tests/test_flexibility.py
@@ -3,12 +3,16 @@ Unit and regression test for the flexibility package.
 """
 
 # Import package, test suite, and other packages as needed
+
+import flexibility
+
 import sys
 
 def test_flexibility_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "flexibility" in sys.modules
 
-## def test_stretch():
    # test = stretch.canvas("hello")
-   #  assert "h e l l o" == test
+def test_stretch():
+    test = flexibility.stretch.canvas("hello")
+    assert "h e l l o" == test
