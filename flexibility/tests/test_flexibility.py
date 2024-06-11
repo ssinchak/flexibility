32e6e8eb4abac2c607c4f80ea799b79bb37d593f
ssinchak <shs355@pitt.edu>
2024-06-11 13:50:54 -0700

    fix

--- flexibility/tests/test_flexibility.py
+++ flexibility/tests/test_flexibility.py
@@ -12,7 +12,6 @@ def test_flexibility_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "flexibility" in sys.modules
 
-   # test = stretch.canvas("hello")
 def test_stretch():
     test = flexibility.stretch.canvas("hello")
     assert "h e l l o" == test
